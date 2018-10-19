207543cfea3436bc95c61f020168b1c2c61671df
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2018-10-19 18:33:51 +0200

    created new test case

--- test/irods_mock.py
+++ test/irods_mock.py
@@ -2,6 +2,7 @@ import os
 import json
 from collections import namedtuple
 from irods.exception import CollectionDoesNotExist
+from irods.exception import DataObjectDoesNotExist
 
 
 class iRodsMetaData(object):
@@ -17,29 +18,79 @@ class iRodsMetaData(object):
         return [i.name for i in self._items]
 
 
-class iRodsCollection(object):
-    def __init__(self, obj):
+class iRodsDataObject(object):
+    def __init__(self, parent, obj):
+        self.collection = parent
+        self.name = obj.get('name')
         self.metadata = iRodsMetaData(obj.get('metadata', []))
-        self.subcollections = [iRodsCollection(s)
+
+    @property
+    def size(self):
+        return os.path.getsize(self.real_path)
+
+    @property
+    def path(self):
+        return os.path.join(self.collection.ipath, self.name)
+
+    @property
+    def real_path(self):
+        return os.path.join(self.collection.session.collection_dir,
+                            self.collection.ipath.replace('/', '#'),
+                            self.name)
+
+
+class iRodsCollection(object):
+    def __init__(self, session, ipath, obj, parent=None):
+        self.parent = parent
+        self.session = session
+        self.ipath = ipath
+        self.metadata = iRodsMetaData(obj.get('metadata', []))
+        self.subcollections = [iRodsCollection(session, s, parent=self)
                                for s in obj.get('subcollections', [])]
-        self.data_objects = []
+        self.data_objects = [iRodsDataObject(self, o)
+                             for o in obj.get('data_objects', [])]
 
 
 class iRodsCollectionsMock(object):
-    def __init__(self):
-        self.collection_dir = os.path.dirname(os.path.abspath(__file__))
+    def __init__(self, session):
+        self.session = session
+        self.collections = {}
 
     def get(self, ipath):
-        fname = os.path.join(self.collection_dir,
-                             ipath.replace('/', '#') + ".json")
-        if os.path.isfile(fname):
-            with open(fname) as fp:
-                return iRodsCollection(json.load(fp))
-        else:
-            raise CollectionDoesNotExist(ipath)
+        if ipath not in self.collections:
+            fname = os.path.join(self.session.collection_dir,
+                                 ipath.replace('/', '#') + ".json")
+            if os.path.isfile(fname):
+                with open(fname) as fp:
+                    coll = iRodsCollection(self.session,
+                                           ipath,
+                                           json.load(fp))
+                    self.collections[ipath] = coll
+            else:
+                raise CollectionDoesNotExist(ipath)
+        return self.collections[ipath]
+
+
+class iRodsDataObjectsMock(object):
+    def __init__(self, session):
+        self.session = session
+
+    def get(self, path):
+        name = os.path.basename(path)
+        coll = self.session.collections.get(os.path.dirname(path))
+        for obj in coll.data_objects:
+            if obj.name == name:
+                return obj
+        raise DataObjectDoesNotExist(path)
+
+    def open(self, path, mode):
+        obj = self.get(path)
+        return open(obj.real_path, mode)
 
 
 class iRodsSessionMock(object):
     def __init__(self):
-        self.collections = iRodsCollectionsMock()
+        self.collection_dir = os.path.dirname(os.path.abspath(__file__))
+        self.collections = iRodsCollectionsMock(self)
+        self.data_objects = iRodsDataObjectsMock(self)
         self.host = "http://localhost"
--- test/test_irods_collection.py
+++ test/test_irods_collection.py
@@ -14,3 +14,15 @@ class TestIRodsCollection(unittest.TestCase):
         self.assertEqual(coll.getMetaData(), {})
         self.assertEqual(coll.getMetaDataByKey("key"), {})
         self.assertEqual([f for f in coll.downloadCollection()], [])
+
+    def test_read_collection_with_data(self):
+        session = iRodsSessionMock()
+        coll = iRodsCollection("collection_with_data", session)
+        self.assertEqual(coll.uri,
+                         "irods://http://localhost/collection_with_data")
+        self.assertEqual(coll.size(), 30)
+        self.assertFalse(coll.isPublished(['REPO/KEY']))
+        self.assertTrue(coll.validate())
+        self.assertEqual(coll.getMetaData(), {})
+        self.assertEqual(coll.getMetaDataByKey("key"), {})
+        self.assertEqual(len([f for f in coll.downloadCollection()]), 3)
