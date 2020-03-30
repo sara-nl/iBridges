b23e7519b273103dfa14f89bc03ad923c671514f
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2020-03-30 20:27:11 +0200

    IRD-698: fixed

--- iBridges/connection/mongo.py
+++ iBridges/connection/mongo.py
@@ -4,6 +4,36 @@ from pymongo import ASCENDING
 from .ibridges import iBridgesConnection
 
 
+def encode_struct(document):
+    def encode_str(k):
+        k = k.replace("\\", "\\\\")
+        k = k.replace("$", "\\u0024")
+        k = k.replace(".", "\\u002e")
+        return k
+
+    if isinstance(document, dict):
+        return {encode_str(k): encode_struct(v) for k, v in document.items()}
+    elif isinstance(document, list):
+        return [encode_struct(item) for item in document]
+    else:
+        return document
+
+
+def decode_struct(document):
+    def decode_str(k):
+        k = k.replace("\\\\", "\\")
+        k = k.replace("\\u0024", "$")
+        k = k.replace("\\u002e", ".")
+        return k
+
+    if isinstance(document, dict):
+        return {decode_str(k): decode_struct(v) for k, v in document.items()}
+    elif isinstance(document, list):
+        return [decode_struct(item) for item in document]
+    else:
+        return document
+
+
 class iBridgesMongo(iBridgesConnection):
     ARGUMENTS = [('mongodb_url', 'MongoDB connection string'),
                  ('mongodb', 'MongoDB name'),
@@ -22,20 +52,25 @@ class iBridgesMongo(iBridgesConnection):
         collection = self.collection
         collection.create_index([(k, ASCENDING) for k in key],
                                 unique=True)
-        return collection.insert_one(document).inserted_id
+        import pprint
+        pprint.pprint(encode_struct(document))
+        return collection.insert_one(encode_struct(document)).inserted_id
 
     def read(self, key):
-        return self.collection.find_one(key)
+        document = self.collection.find_one(encode_struct(key))
+        return decode_struct(document)
 
     def open(self, key, mode='r'):
         workdir = self.mkdir()
         return open(os.path.join(workdir, '{0}.json'.format(key)), mode)
 
     def update(self, key, sub_doc):
-        obj = {'$set': sub_doc}
+        key = encode_struct(key)
+        obj = {'$set': encode_struct(sub_doc)}
         self.collection.update_one(key, obj)
 
     def remove_if_exists(self, key):
+        key = encode_struct(key)
         self.collection.delete_one(key)
 
     def test_connection(self):
